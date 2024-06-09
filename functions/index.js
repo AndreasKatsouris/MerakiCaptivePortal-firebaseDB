b55c933bd8f405496d1547c542526ecc945d6395
AndreasKatsouris <andreas@askgroupholdings.com>
2024-06-09 13:30:31 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+import { onRequest } from "firebase-functions/v2/https";
+import { info } from "firebase-functions/logger";
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((_request, response) => {
+   info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+ });
