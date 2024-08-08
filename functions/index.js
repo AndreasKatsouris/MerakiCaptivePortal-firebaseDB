2cabb53bd1112f02a2c67907dfc17cbc39b72d19
AndreasKatsouris <andreas@askgroupholdings.com>
2024-08-08 14:35:42 +0200

    8.8.1

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,15 @@
  * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
- 
+ */
 
-import { onRequest } from "firebase-functions/v2/https";
-import { info } from "firebase-functions/logger";
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-export const helloWorld = onRequest((_request, response) => {
-   info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });*/
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
