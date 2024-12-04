edff98b5a607fa2fc40af30938d19db491093bef
AndreasKatsouris <andreas@askgroupholdings.com>
2024-12-04 18:35:30 +0200

    Update twilioClient.js

--- functions/twilioClient.js
+++ functions/twilioClient.js
@@ -2,9 +2,9 @@ const twilio = require('twilio');
 //const CustomHttpClient = require('./CustomHttpClient');
 require('dotenv').config();
 
-const accountSid = process.env.accountSid;
-const authToken = process.env.authToken;
-const twilioPhone = process.env.twilioPhone;
+const accountSid = process.env.sid;
+const authToken = process.env.token;
+const twilioPhone = process.env.phone;
 
 if (!accountSid || !authToken || !twilioPhone) {
     console.error("Twilio credentials are not set. Ensure TWILIO_SID, TWILIO_TOKEN, and TWILIO_PHONE are defined in environment variables.");
