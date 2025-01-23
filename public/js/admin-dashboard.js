d8fe4f351d6ab976998241d8a238157bd8085bbf
AndreasKatsouris <andreas@askgroupholdings.com>
2025-01-23 16:06:22 +0200

    Update admin-dashboard.js

--- public/js/admin-dashboard.js
+++ public/js/admin-dashboard.js
@@ -2,7 +2,7 @@ import { updateDashboardStats, initializeDashboardListeners } from './dashboard.
 import { initializeProjectManagement } from './project-management.js';
 import { initializeRewardTypes } from './reward-types.js';
 import { initializeGuestManagement } from './guest-management.js';
-const { initializeCampaignManagement } = require('./campaigns.js');
+import { initializeCampaignManagement }from './campaigns.js';
 
 window.addEventListener('error', function(e) {
     console.error('Global error:', e.error);
