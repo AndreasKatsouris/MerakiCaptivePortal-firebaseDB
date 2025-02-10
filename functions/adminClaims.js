1ee14f92a61343572459d41db8b89f3fc20bc286
AndreasKatsouris <andreas@askgroupholdings.com>
2025-02-10 21:04:10 +0200

    update

--- functions/adminClaims.js
+++ functions/adminClaims.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 // This function sets admin claim for specified email addresses
 exports.setAdminClaim = functions.https.onCall(async (data) => {
